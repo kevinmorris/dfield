002a06fcc4c107230ed0843bd7a9c3be0334f0fa
Kevin Morris <kmorris511@yahoo.com>
2012-04-21 00:08:30 -0600

    Adding unit tests for varying slopes
    Signed-off-by: Kevin Morris <kmorris511@yahoo.com>

--- dfield_test.py
+++ dfield_test.py
@@ -12,6 +12,20 @@ class DFieldTest(unittest.TestCase):
     self.assertEqual([round(-sqrt(2) / 4, 3), round(sqrt(2) / 4, 3)], [round(i, 3) for i in xs])
     self.assertEqual([round(-sqrt(2) / 4, 3), round(sqrt(2) / 4, 3)], [round(i, 3) for i in ys])
     
+  def testSlopeSegment_slopeHalf_at00(self):
+    
+    xs, ys = slopeSegment(0, 0, 0.5)
+
+    self.assertEqual([-4472.0, 4472.0], [round(i*(10**4)) for i in xs])    
+    self.assertEqual([-2236.0, 2236.0], [round(i*(10**4)) for i in ys])
+    
+  def testSlopeSegment_slope2_at00(self):
+
+    xs, ys = slopeSegment(0, 0, 2)
+
+    self.assertEqual([-2236.0, 2236.0], [round(i*(10**4)) for i in xs])    
+    self.assertEqual([-4472.0, 4472.0], [round(i*(10**4)) for i in ys])    
+    
 
 
 if __name__ == '__main__':
